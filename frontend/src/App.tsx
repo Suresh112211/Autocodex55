2b3ff80aae948daee588a30388007fd5712f385e
Suresh <adanisuresh@gmail.com>
2025-12-06 16:18:12 +0530

    add frontend/src/App.tsx with starter React code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: 40 }}>
+      <h1>Welcome to AutoCodeX</h1>
+      <p>World's Most Advanced, FREE, Unlimited App & Website Generator</p>
+      <p>Start building your app or site!</p>
+    </div>
+  );
+}
+
+export default App;
