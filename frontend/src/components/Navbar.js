1ec4718557767e1104f50eb383dd208d70d28232
Suresh <adanisuresh@gmail.com>
2025-12-08 18:08:56 +0530

    Add frontend/src/components/Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
